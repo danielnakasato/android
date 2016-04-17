609157690738d05e4ba2a1a34c92d5507c4912f3
Thiago Oliveira <thiago.soliveira@gmail.com>
2016-04-17 11:55:24 -0300

    Created base project strucutre

---
+++ LibSamples/floatlabeledittext/src/main/java/com/supernova/android/floatlabeledittext/FloatLabelEditText.java
@@ -0,0 +1,23 @@
+package com.supernova.android.floatlabeledittext;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+/**
+ * Created by Thiago on 17/04/2016.
+ */
+public class FloatLabelEditText extends EditText {
+
+    public FloatLabelEditText(Context context) {
+        super(context);
+    }
+
+    public FloatLabelEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FloatLabelEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
