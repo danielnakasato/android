bdf7bccc3e02caa6ae8fcd570cab4c16c6bc6604
Thiago Oliveira <thiago.soliveira@gmail.com>
2016-04-17 12:45:25 -0300

    Added class to bottom sheet view module

---
+++ LibSamples/bottomsheetview/src/main/java/com/supernova/android/bottomsheetview/BottomSheetView.java
@@ -0,0 +1,23 @@
+package com.supernova.android.bottomsheetview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Thiago on 4/17/2016.
+ */
+public class BottomSheetView extends LinearLayout {
+
+    public BottomSheetView(Context context) {
+        super(context);
+    }
+
+    public BottomSheetView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BottomSheetView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
