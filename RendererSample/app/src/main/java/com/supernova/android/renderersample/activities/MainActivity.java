ed5c93a39e496228edca0eed51593b84f513d699
Thiago Oliveira <thiago.soliveira@gmail.com>
2016-04-21 21:20:00 -0300

    Changed to grid layout manager

--- RendererSample/app/src/main/java/com/supernova/android/renderersample/activities/MainActivity.java
+++ RendererSample/app/src/main/java/com/supernova/android/renderersample/activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.supernova.android.renderersample.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
@@ -20,6 +21,7 @@ import com.supernova.android.renderersample.renderers.VideoRenderer;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,18 +38,30 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.fab_add).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Video video = new Video();
-                video.setTitle(String.valueOf(mListAdapteeCollection.size()));
-                mAdapter.add(video);
-                mAdapter.notifyItemInserted(mListAdapteeCollection.indexOf(video));
+                int toBeInsertedIndex = getRandom(0, mListAdapteeCollection.size() - 1);
+                Object objectToBeInserted;
+
+                if (getRandom(0, 1) == 0) {
+                    Video video = new Video();
+                    video.setTitle(String.valueOf(mListAdapteeCollection.size()));
+                    objectToBeInserted = video;
+                } else {
+                    Banner banner = new Banner();
+                    banner.setTitle("BANNER");
+                    objectToBeInserted = banner;
+                }
+
+                mListAdapteeCollection.add(toBeInsertedIndex, objectToBeInserted);
+                mAdapter.notifyItemInserted(toBeInsertedIndex);
             }
         });
         findViewById(R.id.fab_remove).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int toBeRemovedIndex = getRandom(0, mListAdapteeCollection.size() - 1);
+
+                mListAdapteeCollection.remove(toBeRemovedIndex);
                 mAdapter.notifyItemRemoved(toBeRemovedIndex);
-                mAdapter.remove(mListAdapteeCollection.get(toBeRemovedIndex));
             }
         });
 
@@ -73,8 +87,21 @@ public class MainActivity extends AppCompatActivity {
         // init recycler view
         SpacesItemDecoration decoration = new SpacesItemDecoration(10);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(2,
-                StaggeredGridLayoutManager.VERTICAL);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                int spanSize;
+
+                if (mListAdapteeCollection.get(position).getClass() == Banner.class) {
+                    spanSize = 2;
+                } else {
+                    spanSize = 1;
+                }
+
+                return spanSize;
+            }
+        });
         recyclerView.addItemDecoration(decoration);
         recyclerView.setLayoutManager(layoutManager);
         mAdapter = new RVRendererAdapter(rendererBuilder,  mListAdapteeCollection);
