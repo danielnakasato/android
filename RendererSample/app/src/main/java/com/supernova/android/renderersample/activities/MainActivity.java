691f7ed3e070960b4abfe8972a53ea49c8adf705
Thiago Oliveira <thiago.soliveira@gmail.com>
2016-04-23 09:54:06 -0300

    Added svg drawables

--- RendererSample/app/src/main/java/com/supernova/android/renderersample/activities/MainActivity.java
+++ RendererSample/app/src/main/java/com/supernova/android/renderersample/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.supernova.android.renderersample.activities;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
